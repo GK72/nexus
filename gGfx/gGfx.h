acff7de95123087a3bd9badf004bf87f05f5a040
GK72 <gk.project72@gmail.com>
2019-09-22 13:16:41 +0200

    (refact) Class Engine - inheritance

--- gGfx/gGfx.h
+++ gGfx/gGfx.h
@@ -109,11 +109,11 @@ struct KeyState {
 };
 
 #ifdef _MSC_BUILD
-class Engine
+class EngineConW : public EngineGFX
 {
 public:
-    Engine(int width, int height, int fontWidth, int fontHeight);
-    virtual ~Engine();
+    EngineConW(int width, int height, int fontWidth, int fontHeight);
+    virtual ~EngineConW();
 
     void run();
     void draw(const Point2D& p, short color) const;
@@ -178,7 +178,7 @@ public:
     ~Sprite()				{ delete[] glyph; delete[] colour; }
 
     void setColour(COLOUR colour);
-    void draw(const Engine& gfx, int x, int y) const;
+    void draw(const EngineConW& gfx, int x, int y) const;
 
 private:
     wchar_t* glyph = nullptr;
