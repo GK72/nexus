9f51a5c69dc6e2e3ca566ffc4341fc0b7c2df5c4
GK72 <gk.project72@gmail.com>
2019-09-22 10:56:35 +0200

    (imp) Added NCurses library

--- console/Console.cpp
+++ console/Console.cpp
@@ -9,15 +9,50 @@
 #include "gmath.h"
 #include "gGfx.h"
 #include "utility.h"
+#include "curses.h"
 //#include "libgs.h"              // DLL library
 
+int r, c,    // current row and column (upper-left is (0,0))
+    nrows, // number of rows in window
+    ncols; // number of columns in window
 
+void draw(char dc)
+{
+    move(r, c); // curses call to move cursor to row r, column c
+    delch();
+    insch(dc);    // curses calls to replace character under cursor by dc
+    refresh(); // curses call to update screen
+    r++;       // go to next row
+               // check for need to shift right or wrap around
+        if (r == nrows)
+    {
+        r = 0;
+        c++;
+        if (c == ncols) c = 0;
+    }
+    }
 
 int main()
 {
-    std::string ip("fe80:0000:0000:0000:fc30:35fb:e172:4230");
-    std::cout << glib::ipv6Formatter(ip);
+    int i;
+    char d;
+    WINDOW *wnd;
+    wnd = initscr();          // curses call to initialize window
+    cbreak();                    // curses call to set no waiting for Enter key
+    noecho();                    // curses call to set no echoing
+    ncols = getmaxx(wnd);
+    nrows = getmaxy(wnd);
+    //getmaxyx(wnd, nrows, ncols); // curses call to find size of window
+    clear();                     // curses call to clear screen, send cursor to position (0,0)
+    refresh();                   // curses call to implement all changes since last refresh
+    r = 0;
+    c = 0;
+    while (1)
+    {
+        d = getch();         // curses call to input from keyboard
+        if (d == 'q') break; // quit?
+        draw(d);             // draw the character
 
-
-	return 0;
+    }
+    endwin(); // curses call to restore the original window and leave
 }
