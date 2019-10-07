2f3d0f03626b66f6b685590bf6ae15054861b4b4
Gábor Krisztián Girhiny <gk.project72@gmail.com>
2019-10-07 19:51:52 +0200

    (imp) Publisher-Subscriber pattern and Event struct

--- gGfx/gGfx.h
+++ gGfx/gGfx.h
@@ -179,9 +179,6 @@ protected:
     CHAR_INFO* screenBuffer;
 
 private:
-    static std::atomic<bool> atomActive;
-    std::mutex mx;
-    std::condition_variable cv;
     bool hasInputEvent = true;                  // Because the first frame needs to be rendered
 
     short screenWidth;
--- rpg/rpg.cpp
+++ rpg/rpg.cpp
@@ -9,6 +9,8 @@
 
 #include "rpg.h"
 
+std::mutex mx;
+
 namespace glib {
 namespace rpg {
 
@@ -36,19 +38,20 @@ void Game::run()
 
 void Game::init()
 {
-    hero = new Hero("Aida");
+    mt = std::mt19937(rd());
+    hero = new Hero(this, "Aida");
     glib::gGfx::FrameBuilder framebuilder(gfx);
-    frames.push_back(framebuilder.createFrame("Main", Point2D(32, 8)));
-    frames.push_back(framebuilder.createFrame("Map", Point2D(32, 16), Point2D(50, 0)));
+    frames["Main"] = framebuilder.createFrame("Main", Point2D(32, 8));
+    frames["Map"] = framebuilder.createFrame("Map", Point2D(32, 16), Point2D(50, 0));
 
-    new glib::gGfx::FrameContentText(frames[0], hero->toString());
+    new glib::gGfx::FrameContentText(frames["Main"], hero->toString() + "\nAttack: ");
 }
 
 int Game::inputHandling()
 {
-    int inputEventNo = 0;
+    inputEventNo = 0;
     if (gfx->getKey(VK_SPACE).isPressed) {
-        outstr = "SPACE was pressed";
+        hero->attack();
         ++inputEventNo;
     }
 
@@ -57,17 +60,31 @@ int Game::inputHandling()
 
 void Game::update(float elapsedTime)
 {
+    // Fetch event message and dispatch it
+
     for (const auto& e : frames) {
-        e->draw();
+        e.second->draw();
     }
 }
 
-
-Hero::Hero(std::string name)
+void Game::trigger(Event& evt)
 {
+    frames["Main"]->setContent(hero->toString() + "\nAttack: " + evt.msg);
+}
+
+
+
+Hero::Hero(Game* engine, std::string name)
+{
+    _engine = engine;
     _name = name;
     _heroLevel = 1;
     _experience = 0;
+
+    _damageMin = 1;
+    _damageMax = 5;
+
+    attach(engine);
 }
 
 std::string Hero::toString() const
@@ -77,6 +94,19 @@ std::string Hero::toString() const
         "Level: " + std::to_string(_heroLevel) + '\n' +
         "Exp  : " + std::to_string(_experience)
     );
+}
+
+void Hero::move(glib::gGfx::DIRECTION_2D direction)
+{
+
+}
+
+int Hero::attack()
+{
+    std::uniform_int_distribution<> dist(_damageMin, _damageMax);
+    int dmg = dist(_engine->rng());
+    notify(Event(std::to_string(dmg)));
+    return dmg;
 }
 
 
