26f3c7e30905e7470e0a799340a7bf0c4182d27a
GK72 <gk.project72@gmail.com>
2019-10-14 20:07:53 +0200

    (new) Script: config.sh

---
+++ config.sh
@@ -0,0 +1,4 @@
+curl -L https://github.com/gokcehan/lf/releases/download/r13/lf-linux-amd64.tar.gz | tar xzC ~/.local/bin
+chmod +x lf
+sudo mv lf /usr/local/bin
+# https://github.com/gokcehan/lf/wiki/Tutorial
