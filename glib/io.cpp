1f8b368440fe28c64624db7a85be96ca6ce262d7
Gábor Krisztián Girhiny <gk.project72@gmail.com>
2019-10-27 13:17:34 +0100

    [LIB/IO](imp) Recursive JSON reading

--- glib/io.cpp
+++ glib/io.cpp
@@ -1,3 +1,11 @@
+// **********************************************
+// ** gkpro @ 2019-10-27                       **
+// **                                          **
+// **           ---  G-Library  ---            **
+// **            IO implementation             **
+// **                                          **
+// **********************************************
+
 #include "io.h"
 
 
@@ -71,36 +79,76 @@ ParserJSON::ParserJSON(const std::string_view& path)
     m_path = path;
     m_inf = std::ifstream(m_path);
     m_tokenizer = new Tokenizer(std::vector<std::string>{":", ","}, "}");
+    m_data = new container();
+}
+
+ParserJSON::~ParserJSON()
+{
+    delete m_data;
+    delete m_tokenizer;
+}
+
+ParserJSON::container* ParserJSON::read()
+{
+    while ((m_record = readRecord()).size() > 0) {
+        m_data->push_back(m_record);
+    }
+    return m_data;
 }
 
 ParserJSON::record ParserJSON::readRecord()
 {
+    // Sample
+    // {"key": "value", ..., "key": {"key": ... } }
     m_record.clear();
-    m_tokenizer->clear();
+    //m_tokenizer->clear();
 
     if (!m_inf.eof()) {
         std::stringstream ss;
         char ch;
         gint level = 0;
+        bool doRead = true;
 
         // TODO: tokenize also records
         // Finding the start of the record
-        while (m_inf.good()
-            && (ch = m_inf.get()) != '{'
-        );
-        if (ch == '{') { ++level; }
+        while ((ch = m_inf.get()) != '{') {
+            if (!m_inf.good()) {
+                doRead = false;
+                break;
+            }
+        }
+        ++level;
 
-        bool doRead = true;
         while (doRead) {
             ss << (ch = m_inf.get());
-            if (ch == '{')      { ++level; }
-            else if (ch == '}') {
+            std::cout << ch;
+            if (ch == '{') {
+                ++level;
+                // Stopping in between a key and value (value is the new record)
+                gint end = ss.str().find_last_of(",");
+                std::string str = ss.str().substr(0, end);
+                // Store key value pairs in a string
+                //m_tokenizer->setString(str);
+                //readKeyValuePair();
+
+                str = ss.str().substr(end + 1);
+                m_tokenizer->setString(str);
+                std::string key;
+                key = readToken();
+                m_inf.putback(ch);
                 --level;
+                std::any value = readRecord();
+                m_record.clear();
+                m_record[key] = value;
+            }
+            else if (ch == '}') {
+                if (--level == 0) { doRead = false; }
                 m_tokenizer->setString(ss.str());
                 readKeyValuePair();
             }
-            if (level == 0)  { doRead = false; }
         }
+        //m_tokenizer->setString(ss.str());
+        //readKeyValuePair();
     }
     return m_record;
 }
@@ -108,7 +156,7 @@ ParserJSON::record ParserJSON::readRecord()
 void ParserJSON::readKeyValuePair()
 {
     std::string key;
-    std::string value;
+    std::any value;
     while ((key = readToken()).size() > 0) {
         value = readToken();
         m_record[key] = value;
