7d7123d4edf423951cc7c0f75739e8d56467501d
Gábor Krisztián Girhiny <gk.project72@gmail.com>
2020-01-29 20:50:16 +0100

    [LIB/ARG](refact) Exceptions moved into namespace ex

--- glib/arg.cpp
+++ glib/arg.cpp
@@ -1,5 +1,13 @@
-#include <iostream>
+// **********************************************
+// ** gkpro @ 2020-01-29                       **
+// **                                          **
+// **           ---  G-Library  ---            **
+// **         Arguments implementation         **
+// **                                          **
+// **********************************************
+
 #include "arg.h"
+#include "utility.h"
 
 namespace glib {
 
@@ -25,7 +33,7 @@ void ArgParser::argsToString(char* argv[])
 
 void ArgParser::displayHelp()
 {
-    std::cout << "Help\n";
+    glib::printn("Help");
 }
 
 void ArgParser::process()
@@ -42,18 +50,18 @@ void ArgParser::parseArgs()
 {
     for (gint i = 0; i < m_inArgs.size(); ++i) {
         try {
-            Arg& arg = m_args.at(m_inArgs[i]);
+            Arg& arg = m_args.at(m_inArgs[i].substr(2));
             arg.isActive = true;
             if (!arg.isFlag) {
                 ++i;
                 if (i >= m_inArgs.size()) {
-                    throw ValuelessArgException(m_inArgs[i - 1]);
+                    throw ex::ValuelessArgException(m_inArgs[i - 1]);
                 }
                 arg.value = m_inArgs[i];
             }
         }
         catch (std::out_of_range) {
-            throw ArgParsingException("'" + m_inArgs[i] + "' is invalid argument");
+            throw ex::ArgParsingException("'" + m_inArgs[i] + "' is invalid argument");
         }
     }
 }
@@ -62,7 +70,7 @@ void ArgParser::checkArgs()
 {
     for (const auto& e : m_args) {
         if (e.second.isRequired && !e.second.isActive) {
-            throw InactiveArgException("'" + e.second.name + "' argument is required");
+            throw ex::InactiveArgException("'" + e.second.name + "' argument is required");
         }
     }
 }
