5181be909a0d104e68c56676202a4f1374071d7c
GK72 <gk.project72@gmail.com>
2020-05-12 20:55:14 +0200

    [LIB/Units](imp) Relational operators
    - type trait
    - helper types
    - first arithmetic operator
    - first literals

--- include/units.h
+++ include/units.h
@@ -5,7 +5,7 @@
 
 namespace glib::units {
 
-template <class Rep, class Period = std::ratio<1>> class Unit;
+template <class Rep, class Measure = std::ratio<1>> class Unit;
 
 template <class R1, class R2>
 struct _ratioGCD
@@ -17,22 +17,39 @@ struct _ratioGCD
 
 }
 
-// Common type specialization
+// -----------------------------==[ Common_Type Specialization ]==----------------------------------
 namespace std {
 
-template <class Rep1, class Period1, class Rep2, class Period2>
+template <class Rep1, class Measure1, class Rep2, class Measure2>
 struct common_type<
-    glib::units::Unit<Rep1, Period1>,
-    glib::units::Unit<Rep2, Period2>>
+    glib::units::Unit<Rep1, Measure1>,
+    glib::units::Unit<Rep2, Measure2>>
 {
     using type = glib::units::Unit<
         typename common_type<Rep1, Rep2>::type,
-        typename glib::units::_ratioGCD<Period1, Period2>::type>;
+        typename glib::units::_ratioGCD<Measure1, Measure2>::type>;
 };
 
-} // namespace std (common type specialization)
+} // namespace std
 
 namespace glib::units {
+
+// -------------------------------------==[ Type Trait ]==------------------------------------------
+
+template <class T>
+struct _isUnit : std::false_type {};
+
+template <class Rep, class Measure>
+struct _isUnit<Unit<Rep, Measure>> : std::true_type {};
+
+template <class Rep, class Measure>
+struct _isUnit<const Unit<Rep, Measure>> : std::true_type {};
+
+template <class Rep, class Measure>
+struct _isUnit<volatile Unit<Rep, Measure>> : std::true_type {};
+
+template <class Rep, class Measure>
+struct _isUnit<const volatile Unit<Rep, Measure>> : std::true_type {};
 
 // --------------------------------------==[ Casting ]==--------------------------------------------
 
@@ -40,79 +57,76 @@ namespace glib::units {
  * @brief No conversion
  */
 template <class FromUnit, class ToUnit,
-          class Period = typename std::ratio_divide<
-                typename FromUnit::Period,
-                typename ToUnit::Period>::type,
-          bool = Period::num == 1,
-          bool = Period::den == 1>
+          class Measure = typename std::ratio_divide<
+                typename FromUnit::Measure,
+                typename ToUnit::Measure>::type,
+          bool = Measure::num == 1,
+          bool = Measure::den == 1>
 struct _unit_cast;
 
 /**
  * @brief Representation casting
  */
-template <class FromUnit, class ToUnit, class Period>
-struct _unit_cast<FromUnit, ToUnit, Period, /* num = 1 */ true, /* den = 1 */ true>
+template <class FromUnit, class ToUnit, class Measure>
+struct _unit_cast<FromUnit, ToUnit, Measure, /* num = 1 */ true, /* den = 1 */ true>
 {
-    constexpr ToUnit operator()(const FromUnit& x) const {
+    constexpr ToUnit operator()(const FromUnit& x) const noexcept {
         return ToUnit(static_cast<typename ToUnit::Rep>(x.count()));
     }
 };
 
 /**
- * @brief Representation casting and Period down conversion
+ * @brief Representation casting and Measure down conversion
  */
-template <class FromUnit, class ToUnit, class Period>
-struct _unit_cast<FromUnit, ToUnit, Period, /* num = 1 */ true, /* den = 1 */ false>
+template <class FromUnit, class ToUnit, class Measure>
+struct _unit_cast<FromUnit, ToUnit, Measure, /* num = 1 */ true, /* den = 1 */ false>
 {
     using CT =
-        typename std::common_type<
+        typename std::common_type_t<
             typename ToUnit::Rep,
             typename FromUnit::Rep,
-            intmax_t
-        >::type;
-    constexpr ToUnit operator()(const FromUnit& x) const {
+            intmax_t>;
+    constexpr ToUnit operator()(const FromUnit& x) const noexcept {
         return ToUnit(static_cast<typename ToUnit::Rep>(
-            static_cast<CT>(x.count()) / static_cast<CT>(Period::den))
+            static_cast<CT>(x.count()) / static_cast<CT>(Measure::den))
         );
     }
 };
 
 /**
- * @brief Representation casting and Period up conversion
+ * @brief Representation casting and Measure up conversion
  */
-template <class FromUnit, class ToUnit, class Period>
-struct _unit_cast<FromUnit, ToUnit, Period, /* num = 1 */ false, /* den = 1 */ true>
+template <class FromUnit, class ToUnit, class Measure>
+struct _unit_cast<FromUnit, ToUnit, Measure, /* num = 1 */ false, /* den = 1 */ true>
 {
     using CT =
-        typename std::common_type<
+        typename std::common_type_t<
             typename ToUnit::Rep,
             typename FromUnit::Rep,
-            intmax_t
-        >::type;
-    constexpr ToUnit operator()(const FromUnit& x) const {
+            intmax_t>;
+    constexpr ToUnit operator()(const FromUnit& x) const noexcept {
         return ToUnit(static_cast<typename ToUnit::Rep>(
-            static_cast<CT>(x.count()) * static_cast<CT>(Period::num))
+            static_cast<CT>(x.count()) * static_cast<CT>(Measure::num))
         );
     }
 };
 
 /**
- * @brief Representation casting and Period conversion
+ * @brief Representation casting and Measure conversion
  */
-template <class FromUnit, class ToUnit, class Period>
-struct _unit_cast<FromUnit, ToUnit, Period, /* num = 1 */ false, /* den = 1 */ false>
+template <class FromUnit, class ToUnit, class Measure>
+struct _unit_cast<FromUnit, ToUnit, Measure, /* num = 1 */ false, /* den = 1 */ false>
 {
     using CT =
-        typename std::common_type<
+        typename std::common_type_t<
             typename ToUnit::Rep,
             typename FromUnit::Rep,
-            intmax_t
-        >::type;
-    constexpr ToUnit operator()(const FromUnit& x) const {
+            intmax_t>;;
+    constexpr ToUnit operator()(const FromUnit& x) const noexcept {
         return ToUnit(static_cast<typename ToUnit::Rep>(
             static_cast<CT>(x.count())
-                * static_cast<CT>(Period::num)
-                / static_cast<CT>(Period::den))
+                * static_cast<CT>(Measure::num)
+                / static_cast<CT>(Measure::den))
         );
     }
 };
@@ -120,41 +134,59 @@ struct _unit_cast<FromUnit, ToUnit, Period, /* num = 1 */ false, /* den = 1 */ f
 /**
  * @brief Wrapper for casting
  */
-template <class ToUnit, class Rep, class Period>
+template <class ToUnit, class Rep, class Measure>
 inline constexpr
-ToUnit
-unit_cast(const Unit<Rep, Period>& x)
+ToUnit unit_cast(const Unit<Rep, Measure>& x) noexcept
 {
-    return _unit_cast<Unit<Rep, Period>, ToUnit>()(x);
+    return _unit_cast<Unit<Rep, Measure>, ToUnit>()(x);
 }
 
 // ------------------------==[ Relational operator implementations ]==------------------------------
 
-template <class Lhs, class Rhs>
+template <class Lhs, class Rhs>             // ----==[ Different types ]==----
 struct _unitEq {
-    constexpr bool operator()(const Lhs& lhs, const Rhs& rhs) const {
+    [[nodiscard]] constexpr
+    bool operator()(const Lhs& lhs, const Rhs& rhs) const noexcept {
         using CT = typename std::common_type_t<Lhs, Rhs>;
         return CT(lhs).count() == CT(rhs).count();
     }
 };
 
-template <class Lhs>
+template <class Lhs, class Rhs>
+struct _unitLt {
+    [[nodiscard]] constexpr
+    bool operator()(const Lhs& lhs, const Rhs& rhs) const noexcept {
+        using CT = typename std::common_type_t<Lhs, Rhs>;
+        return CT(lhs).count() < CT(rhs).count();
+    }
+};
+
+template <class Lhs>                        // ----==[ Common type ]==----
 struct _unitEq<Lhs, Lhs> {
-    constexpr bool operator()(const Lhs& lhs, const Lhs& rhs) const {
+    [[nodiscard]] constexpr
+    bool operator()(const Lhs& lhs, const Lhs& rhs) const noexcept {
         return lhs.count() == rhs.count();
+    }
+};
+
+template <class Lhs>
+struct _unitLt<Lhs, Lhs> {
+    [[nodiscard]] constexpr
+    bool operator()(const Lhs& lhs, const Lhs& rhs) const noexcept {
+        return lhs.count() < rhs.count();
     }
 };
 
 // -------------------------------------==[ UNIT CLASS ]==------------------------------------------
 
-template <class _Rep, class _Period>
+template <class _Rep, class _Measure>
 class Unit {
-    // static_assert(!__is_duration<_Rep>::value, "A duration representation can not be a duration");
-    // static_assert(__is_ratio<_Period>::value, "Second template parameter of duration must be a std::ratio");
-    // static_assert(_Period::num > 0, "duration period must be positive");
+    static_assert(!_isUnit<_Rep>::value, "A unit representation can not be a unit");
+    // static_assert(__is_ratio<_Measure>::value, "Second template parameter of duration must be a std::ratio");
+    // static_assert(_Measure::num > 0, "duration period must be positive");
 public:
-    using Period = typename _Period::type;
-    using Rep    = _Rep;
+    using Measure = typename _Measure::type;
+    using Rep     = _Rep;
 
     constexpr Unit() = default;
 
@@ -166,8 +198,8 @@ public:
         : m_value(value)
     {}
 
-    template <class Rep2, class Period2>
-    constexpr explicit Unit(const Unit<Rep2, Period2>& value
+    template <class Rep2, class Measure2>
+    constexpr explicit Unit(const Unit<Rep2, Measure2>& value
         // ,typename std::enable_if<
         //     std::is_arithmetic<Rep2>::value>::type* = 0
         )
@@ -183,11 +215,81 @@ private:
 
 // --------------------------------==[ Relational operators ]==-------------------------------------
 
-template <class Rep1, class Period1, class Rep2, class Period2>
-inline constexpr bool operator==(const Unit<Rep1, Period1>& lhs, const Unit<Rep2, Period2>& rhs) {
-    return _unitEq<Unit<Rep1, Period1>, Unit<Rep2, Period2>>()(lhs, rhs);
+template <class Rep1, class Measure1, class Rep2, class Measure2>
+[[nodiscard]] inline constexpr
+bool operator==(const Unit<Rep1, Measure1>& lhs, const Unit<Rep2, Measure2>& rhs) noexcept {
+    return _unitEq<Unit<Rep1, Measure1>, Unit<Rep2, Measure2>>()(lhs, rhs);
 };
 
+template <class Rep1, class Measure1, class Rep2, class Measure2>
+[[nodiscard]] inline constexpr
+bool operator!=(const Unit<Rep1, Measure1>& lhs, const Unit<Rep2, Measure2>& rhs) noexcept {
+    return !(lhs == rhs);
+};
 
+template <class Rep1, class Measure1, class Rep2, class Measure2>
+[[nodiscard]] inline constexpr
+bool operator<(const Unit<Rep1, Measure1>& lhs, const Unit<Rep2, Measure2>& rhs) noexcept {
+    return _unitLt<Unit<Rep1, Measure1>, Unit<Rep2, Measure2>>()(lhs, rhs);
+};
+
+template <class Rep1, class Measure1, class Rep2, class Measure2>
+[[nodiscard]] inline constexpr
+bool operator>(const Unit<Rep1, Measure1>& lhs, const Unit<Rep2, Measure2>& rhs) noexcept {
+    return rhs < lhs;
+};
+
+template <class Rep1, class Measure1, class Rep2, class Measure2>
+[[nodiscard]] inline constexpr
+bool operator>=(const Unit<Rep1, Measure1>& lhs, const Unit<Rep2, Measure2>& rhs) noexcept {
+    return !(lhs < rhs);
+};
+
+template <class Rep1, class Measure1, class Rep2, class Measure2>
+[[nodiscard]] inline constexpr
+bool operator<=(const Unit<Rep1, Measure1>& lhs, const Unit<Rep2, Measure2>& rhs) noexcept {
+    return !(lhs > rhs);
+};
+
+// --------------------------------==[ Arithmetic operators ]==-------------------------------------
+
+template <class Rep1, class Measure1, class Rep2, class Measure2>
+[[nodiscard]] inline constexpr
+typename std::common_type_t<Unit<Rep1, Measure1>, Unit<Rep2, Measure2>>
+operator+(const Unit<Rep1, Measure1>& lhs, const Unit<Rep2, Measure2>& rhs) noexcept
+{
+    using CT =
+        typename std::common_type_t<
+            Unit<Rep1, Measure1>,
+            Unit<Rep2, Measure2>>;
+
+    return CT(
+        CT(lhs).count() + CT(rhs).count()
+    );
+}
+
+
+// ------------------------------------==[ Helper Types ]==-----------------------------------------
+
+using bit   = Unit<long long, std::ratio<1, 8>>;
+using byte  = Unit<long long>;
+using kByte = Unit<long long, std::ratio<1024>>;
+using MByte = Unit<long long, std::ratio<1048576>>;
+using GByte = Unit<long long, std::ratio<1073741824>>;
+using TByte = Unit<long long, std::ratio<1099511627776>>;
+
+// --------------------------------------==[ Literals ]==-------------------------------------------
+
+namespace literals {
+    constexpr bit operator""_bit(unsigned long long x) {
+        return bit(static_cast<byte::Rep>(x));
+    }
+
+    constexpr byte operator""_byte(unsigned long long x) {
+        return byte(static_cast<byte::Rep>(x));
+    }
+
+    // TODO ...
+}
 
 } // namespace glib::units
