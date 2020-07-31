4290c0e852aab11eee96b2fc921b3682d23ff490
GK72 <gk.project72@gmail.com>
2020-07-31 14:22:21 +0000

    [CFG](imp) Git aliases

--- cfg/alias.zsh
+++ cfg/alias.zsh
@@ -11,9 +11,20 @@ alias tws='timew start'
 alias twt='timew stop'
 alias twss='timew summary'
 alias twc='timew continue'
+
 alias rsyncp="rsync -avz --info=progress2"
 
-alias gl='git --no-pager log --oneline --decorate --graph -n10'
+alias gap='git add --patch'
+alias gc='git commit'
+alias gca='git commit --amend'
+alias gch='git checkout'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias gds='git --no-pager diff --stat'
+alias gdjq='git-diff-jq'
+alias gl="git --no-pager log --decorate --graph --date=format:'%a %d %b %Y %H:%M:%S' --pretty=tformat:'%C(178)%h %C(029)%cd %C(027)%<(20,trunc)%cn %C(250)%s %C(auto)%d' -n10"
+alias gp='git push origin HEAD:refs/for/master'
+alias gs='git status'
 
 alias clearcache='sudo sh -c "echo 1 > /proc/sys/vm/drop_caches"'
 
