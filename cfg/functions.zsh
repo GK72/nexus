b4c330c165f7ee07f6b6510f97bcaac9382b471b
GK72 <gk.project72@gmail.com>
2021-01-12 21:31:56 +0100

    [TOOLS/SH] find-time-range shell function

--- cfg/functions.zsh
+++ cfg/functions.zsh
@@ -1,3 +1,20 @@
 function colormap() {
     for i in {0..255}; do print -Pn "%K{$i} %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%8)):#7}:+$'\n'}; done
 }
+
+function find-time-range() {
+    PATH_FIND="$1"
+    PATTERN="$2"
+    START_SECONDS=$3
+    RANGE_SECONDS=$4
+    COMMAND="$5"
+
+    MINUTES_AGO=$((($(date +%s)-$START_SECONDS)/60))
+
+    find "${PATH_FIND}" \
+        -name "${PATTERN}" \
+        -cmin -"${MINUTES_AGO}" \
+        -cmin +$(($MINUTES_AGO-($RANGE_SECONDS/60))) \
+        -exec "${(z)COMMAND}" {} \;
+        # -exec $(echo "${COMMAND}") {} \;          # Bash
+}
