bfbe7c6f418f4848e4eaa740bf0dca97ff4c7110
GK72 <gk.project72@gmail.com>
2021-03-11 21:00:15 +0100

    [LIB](imp) Benchmarking - sorting

--- src/tools/bench.cpp
+++ src/tools/bench.cpp
@@ -1,36 +1,53 @@
 #include <benchmark/benchmark.h>
 
 #include <array>
+#include <algorithm>
+#include <list>
+#include <vector>
 
-constexpr size_t SIZE = 1'000;
-
-static void withCond(benchmark::State& state) {
-    auto vec = std::array<int, SIZE>();
-    bool c = true;
-    int sum = 0;
-
+template <class T>
+static void sortList(benchmark::State& state) {
     for (auto _ : state) {
-        for (const auto& x : vec) {
-            if (c) {
-                sum += x;
-            }
-        }
+        state.PauseTiming();
+        std::list<int> list(state.range(0));
+        state.ResumeTiming();
+
+        list.sort();
+        benchmark::DoNotOptimize(list);
     }
 }
 
-BENCHMARK(withCond);
-
-static void withoutCond(benchmark::State& state) {
-    auto vec = std::array<int, SIZE>();
-    int sum = 0;
-
+template <class T>
+static void sortVec(benchmark::State& state) {
     for (auto _ : state) {
-        for (const auto& x : vec) {
-            sum += x;
-        }
+        state.PauseTiming();
+        std::vector<T> vec(state.range(0));
+        state.ResumeTiming();
+
+        std::sort(std::begin(vec), std::end(vec));
+        benchmark::DoNotOptimize(vec.data());
     }
 }
 
-BENCHMARK(withoutCond);
+template <size_t SIZE>
+struct S {
+    char x[SIZE];
+
+    friend bool operator<(const S& lhs, const S& rhs) { return lhs.x[0] < rhs.x[0]; }
+};
+
+
+BENCHMARK_TEMPLATE(sortList, int)->RangeMultiplier(2)->Range(2, 2 << 10);
+BENCHMARK_TEMPLATE(sortVec , int)->RangeMultiplier(2)->Range(2, 2 << 10);
+BENCHMARK_TEMPLATE(sortList, S<8>)->RangeMultiplier(2)->Range(2 << 8, 2 << 10);
+BENCHMARK_TEMPLATE(sortVec , S<8>)->RangeMultiplier(2)->Range(2 << 8, 2 << 10);
+BENCHMARK_TEMPLATE(sortList, S<128>)->Arg(1024);
+BENCHMARK_TEMPLATE(sortVec , S<128>)->Arg(1024);
+BENCHMARK_TEMPLATE(sortList, S<256>)->Arg(1024);
+BENCHMARK_TEMPLATE(sortVec , S<256>)->Arg(1024);
+BENCHMARK_TEMPLATE(sortList, S<512>)->Arg(1024);
+BENCHMARK_TEMPLATE(sortVec , S<512>)->Arg(1024);
+BENCHMARK_TEMPLATE(sortList, S<1024>)->Arg(1024);
+BENCHMARK_TEMPLATE(sortVec , S<1024>)->Arg(1024);
 
 BENCHMARK_MAIN();
