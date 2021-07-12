4a0a60040fc0164a73e20f40bbd22b31e84c8a52
GK72 <gk.project72@gmail.com>
2021-07-12 20:44:12 +0200

    [CFG/TPL] Shell script - Arg handling

--- cfg/vimfiles/templates/bashArgs.sh
+++ cfg/vimfiles/templates/bashArgs.sh
@@ -14,18 +14,21 @@ FWD_ARGS="${NONE}"
 while [[ $# -gt 0 ]]; do
     if [[ "${FWD_ARGS}" == "${NONE}" ]]; then
         case $1 in
-            --)                     FWD_ARGS="";            shift ;;
-            -h | --help)            usage ${0##*/};         exit 0; shift ;;
-            *)                      usage; exit 255;        shift ;;
+            --)                     FWD_ARGS="";;
+            -h | --help)            usage ${0##*/}; exit 0;;
+            -*)                     usage; exit 255;;
+            *)                      break;;
         esac
     else
         FWD_ARGS="${FWD_ARGS}$1 "
         shift
     fi
+    shift
 done
+
+CMD="$@"
 
 # -------------------------------------==[ Initializing ]==-----------------------------------------
 
 
 # -----------------------------------------==[ MAIN ]==---------------------------------------------
-
