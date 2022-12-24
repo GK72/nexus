7598dbd930ec8684410175d17033dcc2a494c61c
GK72 <gk.project72@gmail.com>
2022-12-24 16:17:41 +0100

    [ENV](imp) New Neovim config (Lua)

---
+++ env/nvim/after/plugin/edge.lua
@@ -0,0 +1,19 @@
+vim.cmd[[highlight! link Comment Grey]]
+vim.cmd[[highlight! link DiffText White]]
+
+vim.cmd[[highlight! link TSAttribute Black]]
+vim.cmd[[highlight! link TSFunction Green]]
+vim.cmd[[highlight! link TSNamespace Cyan]]
+vim.cmd[[highlight! link TSNumber Red]]
+vim.cmd[[highlight! link TSParameter Grey]]
+vim.cmd[[highlight! link TSParameterReference Grey]]
+vim.cmd[[highlight! link TSString Green]]
+vim.cmd[[highlight! link TSText Red]]
+vim.cmd[[highlight! link TSMath Red]]
+vim.cmd[[highlight! link TSVariable White]]
+
+vim.cmd[[highlight cursorline cterm=bold ctermbg=236]]
+vim.cmd[[highlight colorcolumn ctermbg=233]]
+vim.cmd[[highlight VertSplit ctermfg=237 ctermbg=234]]
+
+vim.cmd[[highlight normal ctermfg=white]]
---
+++ env/nvim/lua/nexus/init.lua
@@ -0,0 +1,4 @@
+require("nexus.autocmd")
+require("nexus.remaps")
+require("nexus.plugins")
+require("nexus.settings")
