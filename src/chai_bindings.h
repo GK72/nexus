9c8963c9cc69f76723d51604c6571f84ac2825d5
GK72 <gk.project72@gmail.com>
2023-09-10 12:33:45 +0200

    Feat(pt): First steps of path tracing

--- src/chai_bindings.h
+++ src/chai_bindings.h
@@ -9,6 +9,14 @@ public:
     chai();
 
     void eval_file(const std::string& filepath);
+    chaiscript::Boxed_Value eval(const std::string& script);
+
+    template <typename T>
+    void add_ref(T& obj, const std::string& name) {
+        m_chai.add(chaiscript::var(std::ref(obj)), name);
+    }
+
+    chaiscript::ChaiScript& engine() { return m_chai; }
 
 private:
     chaiscript::ChaiScript m_chai;
