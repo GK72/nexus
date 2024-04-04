3a41a02fe7c25419a77ae03e0636f1d027d17d22
GK72 <gk.project72@gmail.com>
2024-04-04 12:53:12 +0200

    Refact(rs): Multiple executables for `nrs_app`

--- env/nvim/after/plugin/lsp.lua
+++ env/nvim/after/plugin/lsp.lua
@@ -56,6 +56,19 @@ local cmp_formatting = {
    }
 }
 
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      -- Hover actions
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
+
 lsp.setup_nvim_cmp({
     mapping = cmp_mappings,
     sources = cmp_sources,
@@ -76,17 +89,4 @@ lsp.setup()
 
 vim.diagnostic.config({
     virtual_text = true,
-})
-
-local rt = require("rust-tools")
-
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
-  },
 })
